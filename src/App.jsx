8f88aead931834c9c90098e4bbc52db55f0091ec
Navansh <khandelwalnavansh1@gmail.com>
2023-06-06 04:19:16 +0000

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Navansh Khandelwal",
+  title: "Web Developer | Competitive Programmer",
+  email: "dev.navansh@gmail.com",
+  gitHub: "navansh",
+  instagram: "",
+  linkedIn: "navansh-khandelwal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "navanshkh",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
